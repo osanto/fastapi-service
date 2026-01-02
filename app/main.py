a9acef46db521271c69785a255e263a419bae1f1
Olha Santo <olha.santo@gmail.com>
2026-01-02 11:45:52 -0600

    add a healthcheck endpoint

--- app/main.py
+++ app/main.py
@@ -14,6 +14,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+@app.get("/")
+def root():
+    return {"status": "healthy", "message": "FastAPI is running"}
+
+
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
